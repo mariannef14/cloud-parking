f52dc58696878d46b7c11de2cd76109e806a1e02
Marianne <mariannefelixs11@gmail.com>
2022-11-23 15:56:20 -0300

    Adicionando testes

--- src/test/java/com/dio/cloudparking/controller/EstacionamentoControllerTest.java
+++ src/test/java/com/dio/cloudparking/controller/EstacionamentoControllerTest.java
@@ -1,8 +1,13 @@
 package com.dio.cloudparking.controller;
 
+import com.dio.cloudparking.dto.EstacionamentoCreateDTO;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import io.restassured.RestAssured;
 
@@ -12,11 +17,93 @@ public class EstacionamentoControllerTest {
     @LocalServerPort
     private int randomPort;
 
+    @BeforeEach
     public void testeConfiguracao(){
         System.out.println(randomPort);
         RestAssured.port = randomPort;
     }
 
     @Test
-    void buscarTodos(){}
+    void quandoEncontrarTodosVerifiqueResultado(){
+        
+        RestAssured.given()
+        .when()
+        .get("/estacionamento/")
+        .then()
+        .statusCode(HttpStatus.OK.value());
+
+    }
+
+    @Test
+    void quandoEncontrarEstacionamentoVerificaResultado(){
+
+        RestAssured.given()
+        .when()
+        .get("/estacionamento/buscarEstacionamento/{id}", "0cb3b694e76e4ceea916b28a8cdb0274")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .body("licenca", Matchers.equalTo("ABC-1605"))
+        .body("modelo", Matchers.equalTo("GOL"))
+        .body("cor", Matchers.equalTo("Vermelho"))
+        .body("estado", Matchers.equalTo("RJ"));
+    }
+
+    @Test
+    void quandoCriarEntaoVerifiqueECriado(){
+
+        EstacionamentoCreateDTO estacionamento = new EstacionamentoCreateDTO();
+        estacionamento.setLicenca("YRH-8745");
+        estacionamento.setModelo("RENEGADE");
+        estacionamento.setCor("Preto");
+        estacionamento.setEstado("RJ");
+
+        RestAssured.given()
+        .when()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(estacionamento)
+        .post("/estacionamento/add")
+        .then()
+        .statusCode(HttpStatus.CREATED.value())
+        .body("licenca", Matchers.equalTo("YRH-8745"))
+        .body("modelo", Matchers.equalTo("RENEGADE"))
+        .body("cor", Matchers.equalTo("Preto"))
+        .body("estado", Matchers.equalTo("RJ"));
+
+    }
+
+    @Test
+    void quandoAtualizarVerificarAtualizacao(){
+
+        EstacionamentoCreateDTO novoEstacionamento = new EstacionamentoCreateDTO();
+        novoEstacionamento.setLicenca("PWL-5001");
+        novoEstacionamento.setModelo("Palio");
+        novoEstacionamento.setCor("Branco");
+        novoEstacionamento.setEstado("PB");
+
+        RestAssured.given()
+        .when()
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .body(novoEstacionamento)
+        .put("/estacionamento/{id}", "6519c1d9648342c88c4f6c7d44ea2448")
+        .then()
+        .statusCode(HttpStatus.OK.value())
+        .body("licenca", Matchers.equalTo("PWL-5001"))
+        .body("modelo", Matchers.equalTo("Palio"))
+        .body("cor", Matchers.equalTo("Branco"))
+        .body("estado", Matchers.equalTo("PB"));
+        
+    }
+
+    
+    @Test
+    void quandoDeletarVerificarResultado(){
+        RestAssured.given()
+        .when()
+        .delete("/estacionamento/deletar/{id}", "6135427b8ced4ae8b7bdf3ff8f1ccd5c")
+        .then()
+        .statusCode(HttpStatus.NO_CONTENT.value());
+    }
 }
+   
+   
+    
